c0d031eff5c5d6522ec5c0f3b5e349df57176a71
shefket-mustafa <miracle_1@abv.bg>
2025-04-23 15:40:13 +0300

    Work logic moved from gameboard comp to main

--- src/components/gameboard/Gameboard.jsx
+++ src/components/gameboard/Gameboard.jsx
@@ -1,27 +1,13 @@
 import { useState } from "react";
 
-const initialGameboard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
+export default function Gameboard({ onSelectSquare, board }) {
 
-export default function Gameboard({ onSelectSquare, turns }) {
-
-    let gameBoard = initialGameboard;
-
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-
-    }
+   
 
   return (
       <ol id="game-board">
 
-        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {board.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol, colIndex)=> <li key={colIndex}>
                     <button onClick={() => onSelectSquare(rowIndex,colIndex)}disabled={playerSymbol !== null}>{playerSymbol}</button></li>)}
